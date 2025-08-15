af785648ddd86d8a75ec13a5ee72a65ba0e3e857
Samarth Kamble <kamblesamarth04@gmail.com>
2025-08-15 23:27:37 +0530

    06: AI Voice Assistant- Implemented the AI Voice agent with VAPI API and setup vapi

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,24 +1,25 @@
 'use client'
-import { Button } from "@workspace/ui/components/button"
+import { useVapi } from '@/modules/widget/hooks/use-vapi'
+import { Button } from '@workspace/ui/components/button'
+import React from 'react'
 
-import { useMutation, useQuery } from "convex/react"
-import { api } from "@workspace/backend/_generated/api"
-
-
-export default function Page() {
-  const users = useQuery(api.users.getMany)
-  const addUser = useMutation(api.users.add)
-
+const Page = () => {
+  const { startCall, endCall, isSpeaking, isConnecting, isConnected, transcript } = useVapi();
 
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello app/widget</h1>
-        <Button size="sm" onClick={() => addUser()}>Add</Button>
+    <div>
+      <Button onClick={() => startCall()}>Start Call</Button>
+      <Button onClick={() => endCall()}>End Call</Button>
+      <div>
+        {isSpeaking && <p>Speaking...</p>}
+        {isConnecting && <p>Connecting...</p>}
+        {isConnected && <p>Connected!</p>}
         <div>
-          {JSON.stringify(users, null, 2)}
+          {JSON.stringify(transcript, null, 2)}
         </div>
       </div>
     </div>
   )
 }
+
+export default Page
---
+++ apps/widget/modules/widget/hooks/use-vapi.ts
@@ -0,0 +1,87 @@
+import Vapi from "@vapi-ai/web";
+import { useEffect, useState } from "react";
+
+interface TranscriptMessage {
+  role: "user" | "assistant";
+  text: string;
+}
+
+export const useVapi = () => {
+  const [vapi, setVapi] = useState<Vapi | null>(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [isConnecting, setIsConnecting] = useState(false);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [transcript, setTranscript] = useState<TranscriptMessage[]>([]);
+
+  useEffect(() => {
+    // Only for testing for Vapi API, otherwise customers will provide their own API key
+    const vapiInstance = new Vapi("e3403532-cd76-48c9-abcf-843bb510220d");
+
+    setVapi(vapiInstance);
+
+    vapiInstance.on("call-start", () => {
+      setIsConnecting(true);
+      setIsConnecting(false);
+      setTranscript([]);
+    });
+
+    vapiInstance.on("call-end", () => {
+      setIsConnected(false);
+      setIsConnecting(false);
+      setIsSpeaking(false);
+    });
+
+    vapiInstance.on("speech-start", () => {
+      setIsSpeaking(true);
+    });
+
+    vapiInstance.on("speech-end", () => {
+      setIsSpeaking(false);
+    });
+
+    vapiInstance.on("error", (error) => {
+      console.log(error, "VAPI_ERROR");
+      setIsConnecting(false);
+    });
+
+    vapiInstance.on("message", (message) => {
+      if (message.type === "transcript" && message.transcriptType === "final") {
+        setTranscript((prev) => [
+          ...prev,
+          {
+            role: message.role === "user" ? "user" : "assistant",
+            text: message.transcript,
+          },
+        ]);
+      }
+    });
+
+    return () => {
+      vapiInstance?.stop();
+    };
+  }, []);
+
+  const startCall = () => {
+    setIsConnecting(true);
+
+    if (vapi) {
+      // Only for testing for VAPI API, otherwise customers will provide their own Assistant ID
+      vapi.start("d37ebe6e-f29b-493f-abc2-d8a1d8bb704e");
+    }
+  };
+
+  const endCall = () => {
+    if (vapi) {
+      vapi.stop();
+    }
+  };
+
+  return {
+    isSpeaking,
+    isConnecting,
+    isConnected,
+    transcript,
+    startCall,
+    endCall,
+  };
+};
