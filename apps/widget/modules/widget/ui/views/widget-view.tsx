7d7def638a939d3b3cf9b5b601282b3851a3f8aa
Samarth Kamble <kamblesamarth04@gmail.com>
2025-08-16 17:28:16 +0530

    10 : Widget Session

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { WidgetFooter } from "../components/widget-footer";
-import { WidgetHeader } from "../components/widget-header";
+// import { WidgetFooter } from "../components/widget-footer";
+// import { WidgetHeader } from "../components/widget-header";
+import WidgetAuthScreen from "../screens/widget-auth-screen";
 
 interface Props {
     organizationId: string;
@@ -11,16 +12,8 @@ const WidgetView = ({organizationId}:Props) => {
   return (
     //  TODO: Confirm whether or not min-h-screen and min-w-screen is needed
     <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-      <WidgetHeader>
-        <div className="flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold">
-          <p className="text-3xl">Hi, there! 👋</p>
-          <p className="text-lg">How can we help you today?</p>
-        </div>
-      </WidgetHeader>
-      <div className="flex flex-1">
-        Widget View: {organizationId}
-      </div>
-     <WidgetFooter />
+      <WidgetAuthScreen />
+     {/* <WidgetFooter /> */}
     </main>
   )
 }
--- packages/backend/convex/schema.ts
+++ packages/backend/convex/schema.ts
@@ -1,7 +1,30 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
+
 export default defineSchema({
+  contactSessions: defineTable({
+    name: v.string(),
+    email: v.string(),
+    organizationId: v.string(),
+    expiredAt: v.number(),
+    metadata: v.optional(v.object({
+      userAgent: v.string(),
+      language: v.string(),
+      languages: v.optional(v.string()),
+      platform: v.optional(v.string()),
+      vendor: v.optional(v.string()),
+      screenResolution: v.optional(v.string()),
+      viewportSize: v.optional(v.string()),
+      timezone: v.optional(v.string()),
+      timezoneOffset:v.optional(v.number()),
+      cookieEnabled: v.optional(v.boolean()),
+      referrer: v.optional(v.string()),
+      currentUrl: v.optional(v.string()),
+    })),
+  })
+  .index('by_organization_id', ['organizationId'])
+  .index('by_expires_at',['expiredAt']),
   users: defineTable({
     name: v.string(),
   }),
