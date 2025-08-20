b33702cd79ae222dfe52d5f4f86b74c3d848efe1
Samarth Kamble <kamblesamarth04@gmail.com>
2025-08-20 18:50:22 +0530

    13 Conversation : Implement the chat screen ui and create chat ui.

--- packages/backend/convex/_generated/api.d.ts
+++ packages/backend/convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as public_contactSessions from "../public/contactSessions.js";
+import type * as public_conversation from "../public/conversation.js";
 import type * as public_organizations from "../public/organizations.js";
 import type * as users from "../users.js";
 
@@ -27,6 +28,7 @@ import type * as users from "../users.js";
  */
 declare const fullApi: ApiFromModules<{
   "public/contactSessions": typeof public_contactSessions;
+  "public/conversation": typeof public_conversation;
   "public/organizations": typeof public_organizations;
   users: typeof users;
 }>;
--- packages/backend/convex/schema.ts
+++ packages/backend/convex/schema.ts
@@ -1,30 +1,45 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
-
 export default defineSchema({
+  conversations: defineTable({
+    threadId: v.string(),
+    organizationId: v.string(),
+    contactSessionId: v.id("contactSessions"),
+    status: v.union(
+      v.literal("unresolved"),
+      v.literal("escalated"),
+      v.literal("resolved")
+    ),
+  })
+    .index("by_organization_id", ["organizationId"])
+    .index("by_contact_session_id", ["contactSessionId"])
+    .index("by_thread_id", ["threadId"])
+    .index("by_status_and_organization_id", ["status", "organizationId"]),
   contactSessions: defineTable({
     name: v.string(),
     email: v.string(),
     organizationId: v.string(),
     expiredAt: v.number(),
-    metadata: v.optional(v.object({
-      userAgent: v.string(),
-      language: v.string(),
-      languages: v.optional(v.string()),
-      platform: v.optional(v.string()),
-      vendor: v.optional(v.string()),
-      screenResolution: v.optional(v.string()),
-      viewportSize: v.optional(v.string()),
-      timezone: v.optional(v.string()),
-      timezoneOffset:v.optional(v.number()),
-      cookieEnabled: v.optional(v.boolean()),
-      referrer: v.optional(v.string()),
-      currentUrl: v.optional(v.string()),
-    })),
+    metadata: v.optional(
+      v.object({
+        userAgent: v.string(),
+        language: v.string(),
+        languages: v.optional(v.string()),
+        platform: v.optional(v.string()),
+        vendor: v.optional(v.string()),
+        screenResolution: v.optional(v.string()),
+        viewportSize: v.optional(v.string()),
+        timezone: v.optional(v.string()),
+        timezoneOffset: v.optional(v.number()),
+        cookieEnabled: v.optional(v.boolean()),
+        referrer: v.optional(v.string()),
+        currentUrl: v.optional(v.string()),
+      })
+    ),
   })
-  .index('by_organization_id', ['organizationId'])
-  .index('by_expires_at',['expiredAt']),
+    .index("by_organization_id", ["organizationId"])
+    .index("by_expires_at", ["expiredAt"]),
   users: defineTable({
     name: v.string(),
   }),
